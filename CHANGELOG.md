bf6f89458a44d6ad35c954065bec308b686bc6e5
Ben Croker <ben@putyourlightson.net>
2020-06-08 14:46:21 +0200

    1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
 # Release Notes for Redactor Aria Label
 
-## 1.0.0 - Unreleased
+## 1.0.0 - 2020-06-08
 - Initial release.
--- src/RedactorAriaLabel.php
+++ src/RedactorAriaLabel.php
@@ -11,9 +11,6 @@ use craft\redactor\events\RegisterPluginPathsEvent;
 use craft\redactor\Field;
 use yii\base\Event;
 
-/**
- * @property ComponentsService $componentsService
- */
 class RedactorAriaLabel extends Plugin
 {
     /**
@@ -28,9 +25,9 @@ class RedactorAriaLabel extends Plugin
             $event->paths[] = __DIR__.'/resources';
         });
 
-        // Allow the use of the aria-label attribute
+        // Allow the use of the aria-label attribute with HTML Purifier
         Event::on(Field::class, Field::EVENT_MODIFY_PURIFIER_CONFIG, function(ModifyPurifierConfigEvent $event) {
-            $event->config->getHTMLDefinition(true)->addAttribute('a', 'aria-label', 'URI');
+            $event->config->getHTMLDefinition(true)->addAttribute('a', 'aria-label', 'Text');
         });
 
         Field::registerRedactorPlugin('aria-label');
