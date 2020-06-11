b2522a8e0f35e68a9fad31287f0001f249956682
Ben Croker <ben@putyourlightson.net>
2020-06-11 17:06:27 +0200

    1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Release Notes for Redactor Aria Label
 
+## 1.0.1 - 2020-06-11
+### Changed
+- Resources are now only included on control panel requests.
+
 ## 1.0.0 - 2020-06-08
 - Initial release.
--- src/RedactorAriaLabel.php
+++ src/RedactorAriaLabel.php
@@ -5,6 +5,7 @@
 
 namespace putyourlightson\redactorarialabel;
 
+use Craft;
 use craft\base\Plugin;
 use craft\redactor\events\ModifyPurifierConfigEvent;
 use craft\redactor\events\RegisterPluginPathsEvent;
@@ -20,16 +21,18 @@ class RedactorAriaLabel extends Plugin
     {
         parent::init();
 
-        // Register resources as Redactor plugin path
-        Event::on(Field::class, Field::EVENT_REGISTER_PLUGIN_PATHS, function(RegisterPluginPathsEvent $event) {
-            $event->paths[] = __DIR__.'/resources';
-        });
+        if (Craft::$app->getRequest()->getIsCpRequest()) {
+            // Register resources as Redactor plugin path
+            Event::on(Field::class, Field::EVENT_REGISTER_PLUGIN_PATHS, function(RegisterPluginPathsEvent $event) {
+                $event->paths[] = __DIR__.'/resources';
+            });
 
-        // Allow the use of the aria-label attribute with HTML Purifier
-        Event::on(Field::class, Field::EVENT_MODIFY_PURIFIER_CONFIG, function(ModifyPurifierConfigEvent $event) {
-            $event->config->getHTMLDefinition(true)->addAttribute('a', 'aria-label', 'Text');
-        });
+            // Allow the use of the aria-label attribute with HTML Purifier
+            Event::on(Field::class, Field::EVENT_MODIFY_PURIFIER_CONFIG, function(ModifyPurifierConfigEvent $event) {
+                $event->config->getHTMLDefinition(true)->addAttribute('a', 'aria-label', 'Text');
+            });
 
-        Field::registerRedactorPlugin('aria-label');
+            Field::registerRedactorPlugin('aria-label');
+        }
     }
 }
